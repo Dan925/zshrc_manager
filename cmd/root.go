5d7991f7148588f4513a33963b46afc64ae4a3bb
Dan925 <dngen029@uottawa.ca>
2026-05-05 12:00:19 -0400

    feat: project skeleton with cobra root command

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var filePath string
+var dryRun bool
+
+var rootCmd = &cobra.Command{
+	Use:   "zshrc",
+	Short: "Manage your .zshrc file safely",
+	Long:  "A CLI tool for listing, searching, and editing your .zshrc with automatic backups.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		home = "~"
+	}
+	rootCmd.PersistentFlags().StringVar(&filePath, "file", home+"/.zshrc", "path to zshrc file")
+	rootCmd.PersistentFlags().BoolVar(&dryRun, "dry-run", false, "show what would happen without making changes")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Dan925/zshrc-manager/cmd"
+
+func main() {
+	cmd.Execute()
+}
